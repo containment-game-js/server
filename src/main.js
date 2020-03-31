dd1764864637207ae78cab4e7b24a18409ef4f26
Guillaume Hivert <hivert.is.coming@gmail.com>
2020-03-31 18:24:52 +0200

    :sparkles: Allow to send state to only one player

--- src/main.js
+++ src/main.js
@@ -100,13 +100,18 @@ const broadcastAction = ({ id, rid, action }) => {
   }
 }
 
-const broadcastState = ({ id, rid, state }) => {
+const broadcastState = ({ id, to, rid, state }) => {
   if (helpers.validateUUID(id)) {
     const room = Rooms.get(rid)
     if (room.host === id) {
       const host = Rooms.getPlayer(room, id)
       if (room && host) {
-        io.to(rid).emit('state', { state })
+        if (to) {
+          const receiver = Rooms.getPlayer(room, to)
+          receiver.socket.emit('state', { state })
+        } else {
+          io.to(rid).emit('state', { state })
+        }
       }
     }
   }
